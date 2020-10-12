ebb96e57e54ff93d30af24ac852bcb1a49371ac8
tacio correia <taciocorreia.tc@gmail.com>
2020-10-12 15:52:55 -0300

    adicionando horas e milesimos

--- src/App.js
+++ src/App.js
@@ -8,22 +8,27 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
+      horas: 0,
       segundos: 0,
       minutos: 0,
+      milisegundos:0,
       stop: false,
       nameStop: "Stop",
-      name: "Relógio", 
+      name: "atual:", 
       parcial: ""
+
     };
   }
+
    zerarCronometro() {
+      this.state.horas = 0
       this.state.segundos = -1
       this.state.minutos = 0
       this.state.parcial = ""
    }
   
   parcial(){
-    let p = this.state.minutos+ ":"+ this.state.segundos + "\n\n"
+    let p = this.state.horas+ ":"+ this.state.minutos + ":"+ this.state.segundos + "\n\n"
     this.state.parcial = this.state.parcial + p
   }
   
@@ -39,47 +44,109 @@ class App extends React.Component {
 
   incrementar () {
     if (this.state.stop === false){
-      this.setState(
+      this.setState(  
          function (state, props) {
-          if (state.segundos >= 5){
-            this.zerar();
+
+
+          if(state.minutos >=4 ){
+            this.zerarMinuto();
+            this.incrementarHora(state);
+         }
+
+          if (state.segundos >=2 ) {
+            this.zerarSegundo();
             this.incrementarMinuto(state);
-          }  
-          return({ segundos: state.segundos +1})
-         })
-    }
+          }
+
+          if(state.milisegundos >=300){
+            this.zerarMiliSegundo();
+            this.incrementarSegundo(state);
+          }
+         return({milisegundos: state.milisegundos +1})
+            
+            
+          } )
+ 
+    }  
   }
-  
-  incrementarMinuto (state) {
-    this.setState(() => { 
-      return {minutos: state.minutos +1}
+ 
+  zerarHora () {
+    this.setState({ 
+      horas: 0 
     })
-  };
-  
-  zerar () {
+  }
+
+  zerarMinuto () {
+    this.setState({ 
+      minutos:0
+    })
+  }
+
+    zerarSegundo () {
     this.setState({ 
       segundos: 0 
     })
   }
 
-  componentDidMount(){
-    this.timer = setInterval(
-      () => this.incrementar(), 1000)
+  zerarMiliSegundo () {
+    this.setState({ 
+      milisegundos:0
+    })
   }
   
+  
+  incrementarHora(state){
+    this.setState(() =>{
+      return {horas: state.horas +1}
+    })
+  };
+  incrementarMinuto (state) {
+    this.setState(() => { 
+      return {minutos: state.minutos +1}
+    })
+  };
+
+  incrementarSegundo (state) {
+    this.setState(() => { 
+      return {segundos: state.segundos +1}
+    })
+  };
+
+  incrementarMiliSegundo (state) {
+    this.setState(() => { 
+      return {milisegundos: state.milisegundos +1}
+    })
+  };
+
+
+
+  componentDidMount(){
+    this.timer = setInterval(
+      () => this.incrementar(), 10);
+  this.timer = setInterval(
+    () => this.incrementar(), 1000);
+  }
+
 
   render(){
 
     return (
       <div>
         
-        <Contador minutos={this.state.minutos} segundos={this.state.segundos} />
+        <Contador horas={this.state.horas} minutos={this.state.minutos} segundos={this.state.segundos} milisegundos={this.state.milisegundos} />
         <LabelRelogio name={this.state.name} />
         <Botao onClick={() => this.zerarCronometro()} label={"Zerar"} />
         <Botao onClick={() => this.pararTempo()} label={this.state.nameStop} />
         <Botao onClick={() => this.parcial()} label={"Pacial"} />
         <LabelRelogio name={this.state.parcial} />
       </div>
+
+
+
+
+
+
+
     );
   }
 }
--- src/Contador.js
+++ src/Contador.js
@@ -3,7 +3,7 @@ import './App.css';
 
 
 const Contador = (props) => (
-    <h1 class="my-title">{props.minutos}:{props.segundos}</h1>
+    <h1 class="my-title">{props.horas}:{props.minutos}:{props.segundos}:{props.milisegundos}</h1>
 )
 
 export default Contador
