8f7fdc16b6f2c2508c20d28d396e437ba6ca8832
Filipe <filipe.100@outlook.com>
2020-10-17 18:18:49 -0300

    customização e modificações no css e no relogio

--- src/Relogio.js
+++ src/Relogio.js
@@ -28,7 +28,64 @@ const Relogio = (props) => {
     const [china, setChina] = React.useState()
     const [australia, setAustralia] = React.useState()
     const [japan, setJapan] = React.useState()
-    
 
+    React.useEffect(() => {
+
+        setInterval(() => {
+            var Brasil = moment.tz("America/Bahia");
+            var Eua = moment.tz("America/New_York");
+            var India = moment.tz("Asia/Colombo");
+            var China = moment.tz("Asia/Macau");
+            var Australia = moment.tz("Australia/Sydney");
+            var Japan = moment.tz("Asia/Tokyo");
+            setBrasil(Brasil.format("DD-MM-YYYY H:m:s"))
+            setEua(Eua.format("DD-MM-YYYY H:m:s"))
+            setIndia(India.format("DD-MM-YYYY H:m:s"))
+            setChina(China.format("DD-MM-YYYY H:m:s"))
+            setAustralia(Australia.format("DD-MM-YYYY H:m:s"))
+            setJapan(Japan.format("DD-MM-YYYY H:m:s"))
+        }, 1000)
+
+    }, [])
+
+    return (
+
+
+        <div class="relo">
+
+            <div>
+                <h1>Relógio</h1>
+            </div>
+            <br></br>
+
+            <h1>Brasília, Brasil:</h1>
+            <h2 className="localTime mb-30">{brasil}</h2>
+
+            <div class="relo2">
+                <Radio.Button onClick={toggleCountries} id="toggleCountries">Outras Localidades</Radio.Button>
+                <div className="d-none" id="countries">
+
+                    <h3>Londres, Reino Unido:</h3>
+                    <p>{eua}</p>
+                    <h3>Nova Iorke, EUA:</h3>
+                    <p>{india}</p>
+                    <h3>Xangai, China:</h3>
+                    <p>{china}</p>
+                    <h3>Paris, França:</h3>
+                    <p>{australia}</p>
+                    <h3>Japan, França:</h3>
+                    <p>{japan}</p>
+                </div>    </div>
+        </div>
+
+
+
+
+
+
+
+
+    )
+}
 
 export default Relogio
